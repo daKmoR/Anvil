d15af5927677af5e441745babf1765709ae6a9e2
Thomas Allmer <d4kmor@gmail.com>
2014-01-10 01:47:17 +0100

    [FEATURE] Restructure for a real App

---
+++ server/startup.js
@@ -0,0 +1,6 @@
+Meteor.startup(function () {
+	// code to run on server at startup
+
+	//Meteor.users.update({_id: 'b3rCTrWY8kNfswQKN'}, {$set: { profile: { name: 'Birgit' }}});
+	//Meteor.users.update({_id: 'pmAJR79vwnXQ9h6NY'}, {$set: { profile: { name: 'Thomas' }}});
+});
