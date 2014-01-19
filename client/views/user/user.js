40bf3c2207cc0912d97ab648b044f07b836efb85
Thomas Allmer <d4kmor@gmail.com>
2014-01-19 18:51:35 +0100

    [WIP] User view with Rank in Statuses

--- client/views/user/user.js
+++ client/views/user/user.js
@@ -3,7 +3,20 @@ Template.user.tasksAssigned = function() {
 };
 
 Template.user.tasksAssignedStatus = function(statusId) {
-	return Tasks.find({assigned: Session.get('displayUserId'), active: false, status: statusId}, {sort: {rank: 1}});
+	var tasksAssignedStatus = [];
+	var above = -1;
+	_.each(Template.user.tasksAssigned().fetch(), function(task) {
+		above++;
+		var found = Tasks.findOne({_id: task._id, assigned: Session.get('displayUserId'), active: false, status: statusId});
+		if (found) {
+			found.above = above * 35;
+			tasksAssignedStatus.push(found);
+			above = -1;
+		}
+	});
+	return tasksAssignedStatus;
+	// standard query without above information
+	// return Tasks.find({assigned: Session.get('displayUserId'), active: false, status: statusId}, {sort: {rank: 1}});
 };
 
 Template.user.statuses = function() {
